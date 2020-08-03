42d91c02977346c873e1a49580cb3903e1e15b20
Mengqi Wang <wmq516@gmail.com>
2020-08-03 16:12:33 -0600

    build all functions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ig = require('./instagram')
 const instagram = async () => {
     await ig.initialize()
     await ig.login('mrwangdli', 'linguistics')
-
+    await ig.likeTagsProcess(['hiking'])
 }
 
 instagram()
--- instagram.js
+++ instagram.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 const BASE_URL = 'https://instagram.com'
-
+const TAG_URL = (tag) => { return `https://www.instagram.com/explore/tags/${tag}/` }
 const instagram = {
     browser: null,
     page: null,
@@ -24,7 +24,53 @@ const instagram = {
         let loginButton = await instagram.page.$x('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button')
         await loginButton[0].click()
 
-        debugger
+        await instagram.page.waitForNavigation({ waitUntil: 'networkidle2' })
+        // click save info button
+        let saveInfoButton = await instagram.page.$x('//*[@id="react-root"]/section/main/div/div/div/div/button')
+        await saveInfoButton[0].click()
+
+        await instagram.page.waitForNavigation({ waitUntil: 'networkidle2' })
+        // click turn on notifications button
+        let NotificationButton = await instagram.page.$x('/html/body/div[4]/div/div/div/div[3]/button[2]')
+        await NotificationButton[0].click()
+
+        await instagram.page.waitForXPath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/span')
+    },
+
+    likeTagsProcess: async (tags = []) => {
+        for (let tag of tags) {
+            // go to the tag page
+            await instagram.page.goto(TAG_URL(tag), { waitUntil: 'networkidle2' })
+            await instagram.page.waitFor(1000)
+
+            let posts = await instagram.page.$$('article > div:nth-child(3) img')
+
+            for (let i = 0; i < 3; i++) {
+                let post = posts[i]
+                // click on the post
+                await post.click()
+
+                // wait for the modal to appear
+                await instagram.page.waitForFunction('document.querySelector("body").style.overflow == "hidden"')
+                await instagram.page.waitFor(2000)
+
+                let isLikeable = await instagram.page.$('svg[aria-label="Like"]')
+
+                if (isLikeable) {
+                    await instagram.page.click('svg[aria-label="Like"]')
+                }
+                await instagram.page.waitFor(3000)
+
+                // Close the modal
+                debugger
+                let closeModalButton = await instagram.page.$x('/html/body/div[4]/div[3]/button')
+                // using xpath selector yields an array that can be selected by index zero. Using $ selector cannot make the button clickable
+                await closeModalButton[0].click()
+                await instagram.page.waitFor(1000)
+            }
+            await instagram.page.waitFor(15000)
+        }
+        await instagram.browser.close()
     }
 }
 
